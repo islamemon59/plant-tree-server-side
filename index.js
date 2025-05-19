6c770bf7cb28e02f7769067b5f02159079b702dc
islamemon59 <islamemon59@gmail.com>
2025-05-19 19:06:18 +0600

    midleware cors dotenv added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const cors = require('cors');
+require('dotenv').config()
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Our Plant Tree Server is running!')

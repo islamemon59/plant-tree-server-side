c1bb6252222d7541788e9475a60dce49a4af8462
islamemon59 <islamemon59@gmail.com>
2025-05-19 19:45:18 +0600

    plant database added

--- index.js
+++ index.js
@@ -21,8 +21,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
     await client.connect();
+
+    const plantCollection = client.db("plantDataBase").collection("plant");
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
